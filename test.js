603f9c1c98c900aef82bf470f20fc5e705cccf54
John <gtom_1984@hotmail.ca>
2022-01-03 23:45:39 -0800

    Create Game Class
    - Will need to test it soon

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ function Player(name) {
 function WordTurn(player_name, words, turn_number) {
     this.player_name = player_name;
     this.words = words;
-    this.words_length = words.length;
+    this.words_length = count_words(this.words);
     this.turn_number = turn_number;
 
     this.get_words = function() {
@@ -30,8 +30,64 @@ function WordTurn(player_name, words, turn_number) {
     }
 }
 
-test_player = new Player("John");
-test_word_turn = new WordTurn(test_player.get_player_name(), "Once upon a time", 10);
-console.log("hello world!");
-console.log(test_player);
-console.log(test_word_turn);
+function Game(num_players, num_turns, max_words) {
+    this.num_players = num_players;
+    this.num_turns = num_turns;
+    this.max_words = max_words;
+
+    this.player_list = [];
+    this.word_turn_list = [];
+    this.current_player_idx = 0;
+    this.current_turn = 0;
+
+    this.initialize_game = function() {
+
+    }
+
+    this.execute_turn = function(fetch_player_words, send_error_message) {
+        
+        
+        let current_player = this.player_list[this.current_player_idx];
+        let word_count = this.max_words + 1;
+        let words = "";
+        while(word_count > this.max_words) {
+            words = fetch_player_words();
+            word_count = count_words(words);
+            if(word_count > this.max_words) {
+                send_error_message("Too many words! Detected number of words: " + word_count);
+            }
+        }
+
+        let current_word_turn = new WordTurn(
+            current_player.name,
+            words,
+            this.current_turn + 1
+        );
+
+        word_turn_list.push(current_word_turn);
+
+        // Checking bounds
+        if(this.current_player_idx > this.num_players - 1) {
+            this.current_player_idx = 0;
+            this.current_turn++;
+        }
+        else {
+            this.current_player_idx++;
+        }
+    }
+
+    this.is_finished = function() {
+        return (this.current_turn)
+    }
+}
+
+// Count words based on number of spaces
+function count_words(words) {
+    // Trim start and end words
+    let trimmed = words.trim();
+    // Replace tabs, multiple spaces with one space
+    trimmed = trimmed.replace(/\s\s+/g, ' ');
+    // A sequence of words would contain [number of spaces] + 1 words.
+    return trimmed.split(" ").length;
+}
+
